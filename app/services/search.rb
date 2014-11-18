87a52937581be6559a8ab8ad8533fecfc57daeba
jmac84242 <jillian.macnaughton@gmail.com>
2014-11-18 21:15:08 +0100

    fixed search code and took out sidekiq

--- app/services/search.rb
+++ app/services/search.rb
@@ -1,20 +1,24 @@
 require 'rest_client'
 require 'nokogiri'
+require "pry"
 
 class Search
 
   def locate_businesses
-    # 'active',
-    categories = ['arts','auto']
-      # ,'beautysvc','education','eventservices','financialservices','food','health','homeservices','hotelstravel','localflavor','localservices','massmedia','nightlife','pets','professional','publicservicesgovt','realestate','religiousorgs','restaurants','shopping']
+    categories = ['active','arts','auto','beautysvc','education','eventservices','financialservices','food','health','homeservices','hotelstravel','localflavor','localservices','massmedia','nightlife','pets','professional','publicservicesgovt','realestate','religiousorgs','restaurants','shopping']
 
-    states = { CA: { "Los_Angeles" => [ 'Beverly_Hills', 'Burbank', 'Culver_City', 'Downtown', 'Encino', 'Glendale', 'Hollywood', 'Koreatown', 'North_Hollywood', 'Pasadena', 'Redondo_Beach', 'Santa_Monica', 'Sherman_Oaks', 'Torrance', 'West_Hollywood', 'West_Los_Angeles' ] } }
+    states = { CA: { "Los_Angeles" => ['Atwater_Village','Bel_Air','Beverley_Crest','Beverly_Grove','Beverly_Hills','Beverlywood','Boyle_Heights','Brentwood', 'Burbank','Canoga_Park','Carthay','Central_Alameda','Century_City','Chatsworth','Chesterfield_Square','Cheviot_Hills','Chinatown','Culver_City','Cypress_Park', 'Downtown', 'Encino', 'Glendale', 'Hollywood', 'Koreatown', 'North_Hollywood', 'Pasadena', 'Redondo_Beach', 'Santa_Monica', 'Sherman_Oaks', 'Torrance', 'West_Hollywood', 'West_Los_Angeles' ] } }
     #todo: each state/city can be it's own sidekiq worker
     categories.each do |category|
+      puts "category #{category}"
       states.each do |state, cities|
+        puts "state #{state}, cities #{cities}"
         cities.each do |city, neighborhoods|
+          puts "city #{cities}, #{neighborhoods}"
           neighborhoods.each do |neighborhood|
-              initial_page_request = RestClient.get("http://www.yelp.com/search?l=p%3A#{state}%3A#{city}%3A%3A#{neighborhood}#find_desc&find_loc&cflt=#{category}&l", "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
+            puts "neighborhood #{neighborhood}"
+              # initial_page_request = RestClient.get("http://www.yelp.com/search?l=p%3A#{state}%3A#{city}%3A%3A#{neighborhood}#find_desc&find_loc&cflt=#{category}&l", "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
+              initial_page_request = RestClient.get("http://www.yelp.com/search?cflt=#{category}&l=p%3A#{state}%3A#{city}%3A%3A#{neighborhood}", "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
 
               # find main("\w*") and string parse for unique ID:
               html_page = Nokogiri::HTML(initial_page_request)
@@ -24,20 +28,87 @@ class Search
                   script.match(/main\(\"\w*\"\)/).to_s
                 end.reject!(&:empty?).last.scan(/\(([^\)]+)\)/).last.first.scan(/\w*/)[1]
               rescue => e
-                Rails.logger.warn "Unable to get parent_request_id: #{e}" 
+                puts "Unable to get parent_request_id: #{e}" 
               end
   
+              puts "#{parent_request_id}"
               # hack-y, ugly temporary fix:
               # parent_request_id = scripts[12].match(/main\(\"\w*\"\)/).to_s.scan(/\(([^\)]+)\)/)[0][0].scan(/\w*/)[1]
           
-              formatted_city_with_state = "#{city.gsub("_", "%20")},%20#{state}"
-              url = "http://www.yelp.com/search/snippet?find_desc&find_loc=#{formatted_city_with_state}&start=20&cflt=active&parent_request_id=#{parent_request_id}&request_origin=user"
+            formatted_city_with_state = "#{neighborhood.gsub("_", "%20")}%20#{city.gsub("_", "%20")},%20#{state}"
+            url = "http://www.yelp.com/search/snippet?find_desc&find_loc=#{formatted_city_with_state}&start=20&cflt=active&parent_request_id=#{parent_request_id}&request_origin=user"
 
-            ParseBusinessesWorker.perform_async(url, formatted_city_with_state, category)
+            #   puts "end of search/start of parse_biz_worker"
+
+            search_results = RestClient.get(url, :user_agent => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36" )
+
+            if !search_results.nil?
+              results = JSON.parse(search_results)
+              doc = Nokogiri::HTML(results["search_results"])
+              next_url = results["seo_pagination"]["relNextUrl"]
+              # if !next_url.empty?
+              #   ParseBusinessesWorker.perform_async(next_url)
+              # end
+
+              doc.css('ul.search-results div.search-result').each do |element|
+                name = element.css('.media-story .biz-name').text
+                puts "****BUSINESS******"
+                puts "name #{name}"
+
+                img_node = element.css('.media-avatar img:first')
+                image = img_node.xpath("@*[starts-with(name(), 'src')]").text
+                puts "image #{image}"
+                data_key = element.xpath("@*[starts-with(name(), 'data-key')]").text.to_i
+
+                full_address_nodeset = element.css('.secondary-attributes address:first')
+                if !full_address_nodeset.empty?
+                  #full_address_nodeset is a nokogiri nodeset with one node.
+                  full_address = full_address_nodeset.first.children
+                  street_address = full_address.first.text.strip 
+                  puts "#{street_address}"
+                  zipcode = full_address.text.split(' ')[-1].to_i
+                  if zipcode.is_a?(Integer)
+                    zipcode = zipcode
+                  end
+                  puts "zipcode = #{zipcode}"
+                  state = full_address.text.split(' ')[-2]
+                  puts "state #{state}"
+                
+                  city_name = city.gsub("_", " ")
+                  puts "city #{city_name}"
+                  category = category
+
+                  exists = Business.where(name: name, address: street_address)
+                  if exists.size <= 0
+                    business = Business.create({
+                      name: name,
+                      address: street_address,
+                      zipcode: zipcode,
+                      city: city_name,
+                      state: state,
+                      image: image,
+                      category: Category.new({name: category})
+                    })
+                  
+                    html_page.css('script').map(&:text)
+                   
+                    map_results = results["search_map"]["markers"]
+                    loc = map_results[data_key.to_s] if map_results.has_key?(data_key.to_s)
+                    puts "loc #{loc}"
+                    if loc.present?
+                      business.location = Location.new(loc['location'])
+                      puts "business location #{business.location}"
+                      business.save
+                    end
+                    puts "#{business.inspect}"
+                  end
+                end
+              end
+            end
+            # ParseBusinessesWorker.perform_async(url, formatted_city_with_state, category)
           end
         end
       end
     end
-
   end
 end
